b32accd71abed3d99ac7d166da967207c3f01d9d
KushagraAgrawal29 <agrawalkushagra29@gmail.com>
2025-05-31 18:23:27 +0530

    fix for user id and next-auth types file

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { getMongoConnection } from "@/lib/mongo";
 import bcrypt from 'bcryptjs'
-import { User } from "@/models/user";
+import User from "@/models/User";
 
 export const authOptions: NextAuthOptions = {
   providers: [
