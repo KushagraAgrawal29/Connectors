14fccae365cd2907209b05a252b1616ccce1fde3
KushagraAgrawal29 <agrawalkushagra29@gmail.com>
2025-06-02 18:09:22 +0530

    page added for signin signup and email verification

---
+++ app/(auth)/onboarding/verify-email/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function VerifyEmail () {
+    return (
+        <div>
+            Email Verify
+        </div>
+    )
+}
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function SignInPage () {
+    return (
+        <div>
+            Sign In
+        </div>
+    )
+}
