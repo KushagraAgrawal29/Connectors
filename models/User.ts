f04b346a796c49ff06ed5b0c68a03affacc86978
KushagraAgrawal29 <agrawalkushagra29@gmail.com>
2025-06-04 23:56:25 +0530

    user is user modal from name to username

--- models/User.ts
+++ models/User.ts
@@ -4,7 +4,7 @@ import mongoose, { Schema, model, models, InferSchemaType } from "mongoose";
 const UserSchema = new Schema(
   {
     email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
   },
